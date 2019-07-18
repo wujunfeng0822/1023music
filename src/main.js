8e279829ae668f2e9fba2b414d94a499600e1be8
1023mo <d1024WEI@163.com>
2019-07-19 07:47:50 +0800

    demo

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import 'styles/reset.css'
+import 'styles/border.css'
 import router from './router'
+import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+
+fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
 
